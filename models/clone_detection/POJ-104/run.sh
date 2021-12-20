2715e3cfa359418520ab3e1a2f8a935e7c492348
jiekeshi <jiekeshi125@gmail.com>
2021-12-20 16:25:40 +0800

    data: data to run defect prediction

--- models/clone_detection/POJ-104/run.sh
+++ models/clone_detection/POJ-104/run.sh
@@ -2,7 +2,7 @@ mkdir logs
 
 CUDA_VISIBLE_DEVICES=1,2 python main.py \
     --do_train \
-    --train_data_file=../../../data/clone_detection/POJ-104/train.jsonl \
+    --train_data_file=../../../data/clone_detection/POJ-104/valid.jsonl \
     --eval_data_file=../../../data/clone_detection/POJ-104/valid.jsonl \
     --epoch 2 \
     --block_size 400 \
