71aadf7501f11734ace17bd2f103442f2080b27d
Jieke SHI <jiekeshi125@gmail.com>
2021-12-24 11:32:10 +0800

    refactor: format code

--- models/code_repair/bleu.py
+++ models/code_repair/bleu.py
@@ -1,17 +1,18 @@
 import collections
 import math
 
+
 def _get_ngrams(segment, max_order):
     ngram_counts = collections.Counter()
     for order in range(1, max_order + 1):
-      for i in range(0, len(segment) - order + 1):
-          ngram = tuple(segment[i:i+order])
-          ngram_counts[ngram] += 1
+        for i in range(0, len(segment) - order + 1):
+            ngram = tuple(segment[i:i+order])
+            ngram_counts[ngram] += 1
     return ngram_counts
 
 
 def compute_bleu(reference_corpus, translation_corpus, max_order=4, smooth=False):
-  
+
     matches_by_order = [0] * max_order
     possible_matches_by_order = [0] * max_order
     reference_length = 0
@@ -22,25 +23,25 @@ def compute_bleu(reference_corpus, translation_corpus, max_order=4, smooth=False
 
         merged_ref_ngram_counts = collections.Counter()
         for reference in references:
-          merged_ref_ngram_counts |= _get_ngrams(reference, max_order)
+            merged_ref_ngram_counts |= _get_ngrams(reference, max_order)
         translation_ngram_counts = _get_ngrams(translation, max_order)
         overlap = translation_ngram_counts & merged_ref_ngram_counts
         for ngram in overlap:
-          matches_by_order[len(ngram)-1] += overlap[ngram]
+            matches_by_order[len(ngram)-1] += overlap[ngram]
         for order in range(1, max_order+1):
-          possible_matches = len(translation) - order + 1
-          if possible_matches > 0:
-            possible_matches_by_order[order-1] += possible_matches
+            possible_matches = len(translation) - order + 1
+            if possible_matches > 0:
+                possible_matches_by_order[order-1] += possible_matches
 
     precisions = [0] * max_order
     for i in range(0, max_order):
         if smooth:
             precisions[i] = ((matches_by_order[i] + 1.) /
-                          (possible_matches_by_order[i] + 1.))
+                             (possible_matches_by_order[i] + 1.))
         else:
             if possible_matches_by_order[i] > 0:
                 precisions[i] = (float(matches_by_order[i]) /
-                            possible_matches_by_order[i])
+                                 possible_matches_by_order[i])
             else:
                 precisions[i] = 0.0
 
