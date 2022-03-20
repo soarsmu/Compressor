36d4eb2df2713e255bcdd088e4831d5eae0ce325
jiekeshi <jiekeshi125@gmail.com>
2022-03-20 19:42:48 +0800

    refactor

--- src/clone_detection/BigCloneBench/distill.sh
+++
@@ -1,7 +0,0 @@
-CUDA_VISIBLE_DEVICES=6 python distill.py \
-    --do_eval \
-    --train_data_file=../../../data/clone_detection/BigCloneBench/train.txt \
-    --eval_data_file=../../../data/clone_detection/BigCloneBench/valid.txt \
-    --block_size 400 \
-    --eval_batch_size 1 \
-    --seed 123456 2>&1| tee logs/eval.log
