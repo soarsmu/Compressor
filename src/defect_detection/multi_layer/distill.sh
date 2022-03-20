fa1b31b1349668c5e323d28eabbb2303648915f8
jiekeshi <jiekeshi125@gmail.com>
2022-03-20 19:45:02 +0800

    feat: add PKD distillation

---
+++ src/defect_detection/multi_layer/distill.sh
@@ -0,0 +1,22 @@
+CUDA_VISIBLE_DEVICES=7 python distill.py \
+    --do_train \
+    --train_data_file=../../../data/defect_detection/train.jsonl \
+    --eval_data_file=../../../data/defect_detection/valid.jsonl \
+    --std_model Roberta \
+    --vocab_size 10000 \
+    --block_size 400 \
+    --train_batch_size 32 \
+    --eval_batch_size 64 \
+    --epochs 10 \
+    --seed 123456 2>&1| tee ../logs/dis.log
+
+CUDA_VISIBLE_DEVICES=7 python distill.py \
+    --do_eval \
+    --train_data_file=../../../data/defect_detection/train.jsonl \
+    --eval_data_file=../../../data/defect_detection/test.jsonl \
+    --std_model Roberta \
+    --vocab_size 10000 \
+    --block_size 400 \
+    --eval_batch_size 64 \
+    --choice best \
+    --seed 123456 2>&1| tee ../logs/dis_eval.log
