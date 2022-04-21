6c5d8dbfbfed76af97eed9ee10830fc8b2e49ec4
jiekeshi <jiekeshi125@gmail.com>
2022-04-21 23:01:33 +0800

    feat: roulette selection

--- src/defect_detection/compress/searcher.py
+++ src/defect_detection/compress/searcher.py
@@ -49,12 +49,13 @@ class Genome(object):
 
 
 class GA_search():
-    def __init__(self, args, search_space, retain_chance=0.3, mutate_chance=0.3):
+    def __init__(self, args, search_space, desired_length=2, mutate_chance=0.2):
         self.args = args
         self.search_space = search_space
-        self.retain_chance = retain_chance
+        self.desired_length = desired_length
         self.mutate_chance = mutate_chance
         self.population = []
+        self.best_gene = []
 
     def is_duplicate(self, new_genome):
         for genome in self.population:
@@ -135,6 +136,8 @@ class GA_search():
         while self.is_duplicate(genome_2):
             genome_2.mutation(self.search_space)
 
+        self.fitness(genome_1)
+        self.fitness(genome_2)
         children.append(genome_1)
         children.append(genome_2)
 
@@ -143,33 +146,55 @@ class GA_search():
     def generation(self):
         for genome in self.population:
             self.fitness(genome)
+
         graded_genome = [x for x in sorted(self.population, key=lambda x: x.fitness, reverse=True)]
+        self.best_gene.append((graded_genome[0].gene_param, graded_genome[0].fitness))
         logger.info(graded_genome[0].gene_param)
         logger.info(graded_genome[0].fitness)
-        retain_length = int(len(graded_genome) * self.retain_chance)
-        new_generation = graded_genome[:retain_length]
-        desired_length = len(self.population) - len(new_generation)
 
         children = []
-        while len(children) < desired_length:
-            parents_id = random.sample(range(len(new_generation)-1), k=2)
-            parents = (new_generation[parents_id[0]], new_generation[parents_id[1]])
-            babies = self.crossover_and_mutation(parents)
+        while len(children) < self.desired_length:
+            parents = random.sample(self.population, 2)
+            children.extend(self.crossover_and_mutation(parents))
 
-            for baby in babies:
-                if len(children) < desired_length:
-                    children.append(baby)
+        self.population.extend(children)
+        fitness_min = min([x.fitness for x in self.population])
+        fitness_sum = sum([x.fitness - fitness_min for x in self.population])
 
-        new_generation.extend(children)
+        fit_ratio = [(x.fitness - fitness_min)/fitness_sum for x in self.population]  # 每个个体占种群适应度的比例
+        fit_ratio_add = [0]  # 个体累计概率
+        for i in fit_ratio:
+            fit_ratio_add.append(fit_ratio_add[len(fit_ratio_add) - 1] + i)     # 计算每个个体的累计概率，并存放到fit_ratio_add中
+        fit_ratio_add = fit_ratio_add[1:]   # 去掉首位的0
+        rand_list = [random.uniform(0, 1) for _ in self.population]     # 生成和种群规模相等的随机值列表，用于轮盘赌选择个体
+        rand_list.sort()
+        fit_index = 0
+        new_index = 0
+        new_population = range(self.desired_length)
+        print(fit_ratio)
+        print(fit_ratio_add)
+        print(rand_list)
+        exit()
+        while new_index < len(self.population):
+            if rand_list[new_index] < fit_ratio_add[fit_index]:
+                new_population[new_index] = self.population[fit_index]
+                new_index = new_index + 1
+            else:
+                fit_index = fit_index + 1
+
+        return new_population
+
+        idx = random.choice(np.arange(POP_SIZE), size=self.args.population_size, replace=True,
+                           p=(fitness) / (fitness.sum()))       # p实际是个数组，大小（size）应该与指定的a相同，用来规定选取a中每个元素的概率，默认为概率相同
         self.population = new_generation
 
 
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--population_size", default=10, type=int, required=True)
-    parser.add_argument("--generation_size", default=20, type=int, required=True)
-    parser.add_argument("--target_size", default=0.01, type=float, required=True)
+    parser.add_argument("--population_size", default=2, type=int)
+    parser.add_argument("--generation_size", default=20, type=int)
+    parser.add_argument("--target_size", default=0.01, type=float)
     parser.add_argument("--target_flops", default=33989813760, type=float)
 
     args = parser.parse_args()
@@ -198,6 +223,7 @@ def main():
 
     logger.info(graded_genome[0].gene_param)
     logger.info(graded_genome[0].fitness)
+    logger.info(searcher.best_gene)
 
 
 if __name__ == "__main__":
