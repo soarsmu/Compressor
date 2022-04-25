e6edd217f8e1ba2698d193ea129c847e7227d365
jiekeshi <jiekeshi125@gmail.com>
2022-04-25 11:14:33 +0800

    feat: data load for clone detection

--- src/clone_detection/compress/utils.py
+++ src/clone_detection/compress/utils.py
@@ -11,7 +11,7 @@ from tokenizers import Tokenizer, models, pre_tokenizers, decoders, trainers, pr
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-def BPE(texts, vocab_size, file_path, logger):
+def BPE(args, texts, vocab_size, file_path, logger):
     tokenizer = Tokenizer(models.BPE(unk_token="<unk>"))
     tokenizer.normalizer = normalizers.Lowercase()
     tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel(add_prefix_space=True)
@@ -26,7 +26,7 @@ def BPE(texts, vocab_size, file_path, logger):
 
     tokenizer.train_from_iterator(texts, trainer)
     folder = "/".join(file_path.split("/")[:-1])
-    tokenizer_path = os.path.join(folder, "BPE" + "_" + str(vocab_size) + ".json")
+    tokenizer_path = os.path.join(folder, "BPE" + "_"+args.type+"_" + str(vocab_size) + ".json")
     tokenizer.save(tokenizer_path, pretty=True)
     logger.info("Creating vocabulary to file %s", tokenizer_path)
     
@@ -42,62 +42,77 @@ class DistilledDataset(Dataset):
         folder = "/".join(file_path.split("/")[:-1])
         cache_file_path = os.path.join(folder, "cached_{}.bin".format(postfix+"_dis_"+str(vocab_size)))
 
-        try:
-            self.examples = torch.load(cache_file_path)
-            logger.info("Loading features from cached file %s", cache_file_path)
-        except:
-            with open("/".join(file_path.split("/")[:-1])+"/data.jsonl") as f:
-                for line in f:
-                    line = line.strip()
-                    js = json.loads(line)
-                    url_to_code[js["idx"]] = js["func"]
+        # try:
+        #     self.examples = torch.load(cache_file_path)
+        #     logger.info("Loading features from cached file %s", cache_file_path)
+        # except:
+        with open("/".join(file_path.split("/")[:-1])+"/data.jsonl") as f:
+            for line in f:
+                line = line.strip()
+                js = json.loads(line)
+                url_to_code[js["idx"]] = js["func"]
 
-            data = []
-            with open(file_path) as f:
-                for line in f:
-                    line = line.strip()
+        data = []
+        with open(file_path) as f:
+            for line in f:
+                line = line.strip()
+                if "train" in postfix:
+                    url1, url2, label, pred = line.split("\t")
+                else:
                     url1, url2, label = line.split("\t")
-                    if url1 not in url_to_code or url2 not in url_to_code:
-                        continue
-                    if label == "0":
-                        label = 0
-                    else:
-                        label = 1
-                    data.append((url1, url2, label))
+                    pred = -1
+                if url1 not in url_to_code or url2 not in url_to_code:
+                    continue
+                if pred == "0":
+                    pred = 0
+                elif pred == "1":
+                    pred = 1
+                else:
+                    pred = -1
 
-            # preds = np.load(os.path.join(folder, "preds_"+postfix+".npy")).astype(int).tolist()
-            preds = np.zeros(len(data)).astype(int).tolist()
-                        # print(len(preds))
-            # print(len(data))
-            assert len(data) == len(preds)
-            mp_data = []
-            for d, pred in tqdm(zip(data, preds)):
-                mp_data.append((d, args, url_to_code, pred))
+                if label == "0":
+                    label = 0
+                elif label == "1":
+                    label = 1
+                elif label == "-1": 
+                    label = pred
+                    # label = -1
 
-            if "train_sampled" in postfix:
-                mp_data = random.sample(mp_data, int(len(mp_data)*0.1))
+                data.append((url1, url2, label, pred, args, url_to_code))
 
-            tokenizer_path = os.path.join(folder, "BPE" + "_" + str(vocab_size) + ".json")
-            
-            if os.path.exists(tokenizer_path):
-                tokenizer = Tokenizer.from_file(tokenizer_path)
-                logger.info("Loading vocabulary from file %s", tokenizer_path)
-            else:
-                texts = []
-                for d in mp_data:
-                    texts.append(" ".join(url_to_code[d[0][0]].split()))
-                    texts.append(" ".join(url_to_code[d[0][1]].split()))
-                tokenizer = BPE(texts, vocab_size, file_path, logger)
+        # # preds = np.load(os.path.join(folder, "preds_"+postfix+".npy")).astype(int).tolist()
+        # preds = np.zeros(len(data)).astype(int).tolist()
+        #             # print(len(preds))
+        # # print(len(data))
+        # assert len(data) == len(preds)
+        # mp_data = []
+        # for d in data:
+        #     mp_data.append((d, args, url_to_code, pred))
 
-            _mp_data = []
-            for d in mp_data:
-                lst = list(d)
-                lst.append(tokenizer)
-                _mp_data.append(tuple(lst))
-    
-            pool = multiprocessing.Pool(multiprocessing.cpu_count())
-            self.examples = pool.map(preprocess, tqdm(_mp_data, total=len(_mp_data)))
-            torch.save(self.examples, cache_file_path)
+        # if "train_sampled" in postfix:
+        #     mp_data = random.sample(mp_data, int(len(mp_data)*0.1))
+
+        tokenizer_path = os.path.join(folder, "BPE" + "_"+args.type+"_" + str(vocab_size) + ".json")
+        
+        if os.path.exists(tokenizer_path):
+            tokenizer = Tokenizer.from_file(tokenizer_path)
+            logger.info("Loading vocabulary from file %s", tokenizer_path)
+        else:
+            texts = []
+            for d in data:
+                texts.append(" ".join(url_to_code[d[0]].split()))
+                texts.append(" ".join(url_to_code[d[1]].split()))
+            tokenizer = BPE(args, texts, vocab_size, file_path, logger)
+
+        _mp_data = []
+        for d in data:
+            lst = list(d)
+            lst.append(tokenizer)
+            _mp_data.append(tuple(lst))
+
+        pool = multiprocessing.Pool(multiprocessing.cpu_count())
+        self.examples = pool.map(preprocess, tqdm(_mp_data, total=len(_mp_data)))
+        # torch.save(self.examples, cache_file_path)
         
     def __len__(self):
         return len(self.examples)
@@ -107,9 +122,9 @@ class DistilledDataset(Dataset):
 
 
 def preprocess(item):
-    d, args, url_to_code, pred, tokenizer = item
-    code1 = " ".join(url_to_code[d[0]].split())
-    code2 = " ".join(url_to_code[d[1]].split())
+    url1, url2, label, pred, args, url_to_code, tokenizer = item
+    code1 = " ".join(url_to_code[url1].split())
+    code2 = " ".join(url_to_code[url2].split())
     code1_ids = tokenizer.encode(code1).ids[:args.block_size-2]
     code2_ids = tokenizer.encode(code2).ids[:args.block_size-2]
     code1_ids = [tokenizer.token_to_id("<s>")]+code1_ids+[tokenizer.token_to_id("</s>")]
@@ -122,7 +137,7 @@ def preprocess(item):
     source_tokens = code1 + code2
     source_ids = code1_ids + code2_ids
 
-    return InputFeatures(source_tokens, source_ids, d[2], pred)
+    return InputFeatures(source_tokens, source_ids, label, pred)
 
 
 def set_seed(seed=42):
